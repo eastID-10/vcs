887aa570b9fa41bab9fc0ce3b16c63e10761fe80
Chandra1620 <148348522+Chandra1620@users.noreply.github.com>
2023-11-05 15:50:30 +0700

    Anggota B kelompok 7

--- SingaLaut.php
+++ SingaLaut.php
@@ -1,14 +1,14 @@
 <?php
 include_once 'HewanLaut.php';
 
-class SingaLaut implements HewanLaut{
-    public function bernapas(): void{
+class SingaLaut implements HewanLaut {
+    public function bernapas(): void {
         echo "Singa Laut Bernapas Dengan Paru Paru <br>";
     }
-    public function berenang(): void{
+    public function berenang(): void {
         echo "Singa Laut Berenang Dengan Memutar sirip belakangnya <br>";
     }
-    public function berkembangbiak(): void{
+    public function berkembangbiak(): void {
         echo "Singa Laut Berkembang Biak Dengan Melahirkan <br>";
     }
 
